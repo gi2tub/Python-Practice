abdc9f9f8935d79f93820941a406fbfac4c77fa3
gi2tub <snobarakhtar25@gmail.com>
2025-01-27 15:30:10 -0800

    my practice

--- python file 3.py
+++ python file 3.py
@@ -1,30 +1,15 @@
+row_1   = ['Facebook',0.0,'USD',23456]
+row_2 = ['Instagram',4.5,'USD',457698]
 
+print(row_1[1])
+print(row_2[-1])
+print(row_1[-3])
+print(row_2[-4])
 
-# Create a list
-my_list = [1, 2, 3, 4, 5]
+num_rating = [row_1[-1],row_2[-1]]
+print(num_rating)
 
-# Print the list
-print(my_list)
-
-# Access an element in the list
-print(my_list[0])  # prints 1
-
-# Add an element to the list
-my_list.append(6)
-print(my_list)  # prints [1, 2, 3, 4, 5, 6]
-
-# Remove an element from the list
-my_list.remove(4)
-print(my_list)  # prints [1, 2, 3, 5, 6]
-
-# Update an element in the list
-my_list[0] = 10
-print(my_list)  # prints [10, 2, 3, 5, 6]
-
-# Get the length of the list
-print(len(my_list))  # prints 5
-
-# Loop through the list
-for item in my_list:
-    print(item)
-
+row_3 = ['Clash',0.0,'USD',4.5]
+print(row_3[:2])
+print(row_3[1:4])
+print(row_3[3:])
