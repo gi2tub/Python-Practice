c283c337a01235428bb79292def1bcd792a8f59f
gi2tub <snobarakhtar25@gmail.com>
2025-01-28 16:19:23 -0800

    my practice

---
+++ new file 4.py
@@ -0,0 +1,15 @@
+row_1 = ['facebook','USD',3.5]
+row_2 = ['Instagram','USD',8.2]
+row_3 = ['Clash',0.0,'USD']
+row_4 = ['Fruit',1.99,'USD']
+lists = [row_1,row_2,row_3,row_4]
+
+print(lists[0][0])
+
+print(lists[1][2])
+print(lists[-2][2])
+print(lists[-1][-1])
+print(lists[:2])
+print(lists[-2:])
+print(lists[1:])
+print(lists[1][:3])
