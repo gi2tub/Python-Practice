ed10a39e4ff4e645e2b06eb946cf64911f1ad32b
gi2tub <snobarakhtar25@gmail.com>
2025-01-30 11:18:52 -0800

    my file

---
+++ Dictonaries.py
@@ -0,0 +1,30 @@
+dictionary = {'key_1':100,'key_2':200}
+dictionary['key_1']
+dictionary['key_2']
+
+
+print(dictionary['key_1'])
+print(dictionary['key_2'])
+
+
+
+dictionary = {'key_1':100,'key_2':200}
+'key_1' in dictionary
+
+'key_5' in dictionary
+
+dictionary = {'key_1':100,'key_2':200}
+
+dictionary['key_1'] += 600
+dictionary['key_2'] +=400
+print(dictionary)
+
+
+
+
+
+
+
+
+
+
---
+++ Functions.py
@@ -0,0 +1,14 @@
+def square(number):
+    return number**6
+print(square(1))
+
+def add(x,y):
+    return x+y
+print(add(4,5))
+
+
+def subtract(b,a):
+    return a-b
+print(subtract(a=4,b=3))
+print(subtract(b=6,a=2))
+print(subtract(b=2,a=10))
