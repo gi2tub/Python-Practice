8dcd16ac593c9e44df203412184a9fb977e00aa8
gi2tub <snobarakhtar25@gmail.com>
2025-02-01 15:51:20 -0800

    This is how we make program of entering password

---
+++ password.py
@@ -0,0 +1,10 @@
+U = "Snobar"
+P = 108
+
+u = str(input("Enter username:"))
+p = int(input("Enter password:"))
+if u==U and p==P:
+    print("Welcome")
+else:
+    print("Incorrect data enterd")
+    
---
+++ program.py
@@ -0,0 +1,39 @@
+def xyz(a,b):
+
+
+    if a%b == 0:
+
+
+        print(a,"a is even number")
+
+    else:
+        print(a,"is odd number")
+        
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+
+xyz(a,b)
+
+
+
+
+        
+
+
+        
+
+
+
+
+        
+
+
+
+
+
+
+
+ 
+
+
+    
